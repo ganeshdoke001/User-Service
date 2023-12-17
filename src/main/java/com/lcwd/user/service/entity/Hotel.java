d7e858462fcac9b89591790767c8545c11abe537
ganeshdoke001 <ganeshdoke1999@gmail.com>
2023-12-17 14:38:47 +0530

    FoignClints configured

---
+++ src/main/java/com/lcwd/user/service/entity/Hotel.java
@@ -0,0 +1,52 @@
+package com.lcwd.user.service.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Hotel {
+
+	private String id;
+	private String name;
+	private String location;
+	private String about;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getAbout() {
+		return about;
+	}
+	public void setAbout(String about) {
+		this.about = about;
+	}
+	public Hotel(String id, String name, String location, String about) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.location = location;
+		this.about = about;
+	}
+	public Hotel() {
+		super();
+	}
+}
--- src/main/java/com/lcwd/user/service/service/UserServiceImpl.java
+++ src/main/java/com/lcwd/user/service/service/UserServiceImpl.java
@@ -1,13 +1,20 @@
 package com.lcwd.user.service.service;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.lcwd.user.service.entity.Hotel;
+import com.lcwd.user.service.entity.Rating;
 import com.lcwd.user.service.entity.User;
 import com.lcwd.user.service.exception.ResourceNotFoundException;
+import com.lcwd.user.service.foignclients.HotelService;
 import com.lcwd.user.service.repo.UserRepo;
 
 @Service
@@ -16,20 +23,50 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	private UserRepo userRepo;
 
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Autowired(required=true)
+	private HotelService hotelService;
+
+	private Logger looger = org.slf4j.LoggerFactory.getLogger(UserServiceImpl.class);
+
 	@Override
 	public User saveUser(User user) {
-	String randomUserId=	UUID.randomUUID().toString();
-	user.setId(randomUserId);
+		String randomUserId = UUID.randomUUID().toString();
+		user.setId(randomUserId);
 		return this.userRepo.save(user);
 	}
 
 	@Override
 	public User getUser(String id) {
-			return userRepo.findById(id).orElseThrow(()-> new ResourceNotFoundException("Resource not found with this id : "+id) );
+		User user = userRepo.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Resource not found with this id : " + id));
+		
+		Rating[] ratingOfUser = this.restTemplate.getForObject("http://RATING-SERVICE/api/ratings/user/" + id,
+				Rating[].class);
+		
+		List<Rating>ratingListobj=Arrays.stream(ratingOfUser).toList();
+		
+		this.looger.info("{}", ratingOfUser);
+		List<Rating> ratingList = ratingListobj.stream().map((rating) -> {
+			
+//			ResponseEntity<Hotel> forEntity = this.restTemplate
+//					.getForEntity("http://HOTEL-SERVICE/api/hotels/" + rating.getHotelId(), Hotel.class);
+//			Hotel hotel = forEntity.getBody();
+			Hotel hotel=this.hotelService.getHotel(rating.getHotelId());
+			
+			this.looger.info("Response status  : {}", hotel);
+			rating.setHotel(hotel);
+			return rating;
+		}).collect(Collectors.toList());
+		user.setRatings(ratingList);
+		return user;
 	}
 
 	@Override
 	public List<User> getAllUsers() {
+
 		return this.userRepo.findAll();
 	}
 
